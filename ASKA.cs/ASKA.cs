aa944118f4b253c44303a84c89de7f961426f367
Gimpy <58477652+tadavispmd040507@users.noreply.github.com>
2025-01-02 18:25:12 -0500

    Minor Fix on the stop process

--- ASKA.cs/ASKA.cs
+++ ASKA.cs/ASKA.cs
@@ -5,9 +5,6 @@ using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Query;
 using WindowsGSM.GameServer.Engine;
 using System.IO;
-using System.Linq;
-using System.Net;
-
 
 namespace WindowsGSM.Plugins
 {
@@ -29,13 +26,11 @@ namespace WindowsGSM.Plugins
         public override string AppId => "3246670"; // Game server appId Steam
 
         // - Standard Constructor and properties
-        public ASKA(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
-        private readonly ServerConfig _serverData;
-        public string Error, Notice;
+        public ASKA(ServerConfig serverData) : base(serverData) => base.serverData = serverData;
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"AskaServer.bat"; // Game server start path
+        public override string StartPath => @"AskaServer.exe"; // Game server start path
         public string FullName = "ASKA Dedicated Server"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
@@ -44,28 +39,28 @@ namespace WindowsGSM.Plugins
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
 
         // - Game server default values
-        public string Port = "7777"; // Default port
-        public string QueryPort = "27015"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
-        
+        public string Port = "27015"; // Default port
+        public string QueryPort = "27016"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+
         // TODO: Unsupported option
         public string Defaultmap = "Default"; // Default map name
 
         // TODO: May not support
         public string Maxplayers = "4"; // Default maxplayers
 
-        //public string Additional = ""; // Additional server start parameter
+        public string Additional = ""; // Additional server start parameter
 
 
         // - Create a default cfg for the game server after installation
         public async void CreateServerCFG()
         {
-            //No config file seems
+            //No editable config file
         }
 
         // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, StartPath);
             if (!File.Exists(shipExePath))
             {
                 Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
@@ -73,19 +68,19 @@ namespace WindowsGSM.Plugins
             }
 
             // Prepare start parameter
-            string param = "";
-            //param += $" {_serverData.ServerParam}";
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -Port={_serverData.ServerPort}"; 
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -ServerQueryPort={_serverData.ServerQueryPort}";
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $" -MaxPlayers={_serverData.ServerMaxPlayer}";
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -Multihome={_serverData.ServerIP}";
+            string param = "-propertiesPath \"server properties.txt\"";
+            //param += $" {serverData.ServerParam}";
+            //param += string.IsNullOrWhiteSpace(serverData.ServerPort) ? string.Empty : $" -Port={serverData.ServerPort}"; 
+            //param += string.IsNullOrWhiteSpace(serverData.ServerQueryPort) ? string.Empty : $" -ServerQueryPort={serverData.ServerQueryPort}";
+            //param += string.IsNullOrWhiteSpace(serverData.ServerMaxPlayer) ? string.Empty : $" -MaxPlayers={serverData.ServerMaxPlayer}";
+            //param += string.IsNullOrWhiteSpace(serverData.ServerIP) ? string.Empty : $" -Multihome={serverData.ServerIP}";
 
             // Prepare Process
             var p = new Process
             {
                 StartInfo =
                 {
-                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    WorkingDirectory = ServerPath.GetServersServerFiles(serverData.ServerID),
                     FileName = shipExePath,
                     Arguments = param,
                     WindowStyle = ProcessWindowStyle.Minimized,
@@ -94,6 +89,7 @@ namespace WindowsGSM.Plugins
                 },
                 EnableRaisingEvents = true
             };
+            p.StartInfo.EnvironmentVariables["SteamAppId"] = "1898300";
 
             // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
             if (AllowsEmbedConsole)
@@ -103,7 +99,7 @@ namespace WindowsGSM.Plugins
                 p.StartInfo.RedirectStandardError = true;
                 p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 p.StartInfo.CreateNoWindow = true;
-                var serverConsole = new ServerConsole(_serverData.ServerID);
+                var serverConsole = new ServerConsole(serverData.ServerID);
                 p.OutputDataReceived += serverConsole.AddOutput;
                 p.ErrorDataReceived += serverConsole.AddOutput;
             }
@@ -127,20 +123,20 @@ namespace WindowsGSM.Plugins
         }
 
 
-// - Stop server function
+        // - Stop server function
         public async Task Stop(Process p)
         {
             await Task.Run(() =>
             {
                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
                 Functions.ServerConsole.SendWaitToMainWindow("^c");
-                p.WaitForExit(2500);
-                Functions.ServerConsole.SendWaitToMainWindow("Y~");
-                p.WaitForExit(500);
+                p.WaitForExit(2000);
+				if (!p.HasExited)
+					p.Kill();
             });
         }
 
-// fixes WinGSM bug, https://github.com/WindowsGSM/WindowsGSM/issues/57#issuecomment-983924499
+        // fixes WinGSM bug, https://github.com/WindowsGSM/WindowsGSM/issues/57#issuecomment-983924499
         public async Task<Process> Update(bool validate = false, string custom = null)
         {
             var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
